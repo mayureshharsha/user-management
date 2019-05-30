f8376771baacc5f7c5ab7c9ce9ef4dd5e202d364
mayureshharsha <hrlive123@gmail.com>
2019-05-30 13:56:00 +0530

    Update CorsConfig.java

--- src/main/java/com/uam/predictionapp/config/CorsConfig.java
+++ src/main/java/com/uam/predictionapp/config/CorsConfig.java
@@ -6,8 +6,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -17,7 +19,10 @@ public class CorsConfig {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://cricpred.s3-website.ap-south-1.amazonaws.com"));
+        List<String> list = new ArrayList();
+        list.add("http://localhost:4200");
+        list.add("http://cricpred.s3-website.ap-south-1.amazonaws.com");
+        config.setAllowedOrigins(list);
         config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
